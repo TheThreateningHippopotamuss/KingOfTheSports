cec3f56956c4176633147fc06f1bf0fb44090b8a
simzzz <swagb3ast@hotmail.com>
2017-09-07 19:38:14 +0300

    Added temporary routing so I can continue working.

--- KingOfTheSports/src/app/app-routing.module.ts
+++ KingOfTheSports/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {path: 'home', component: HomeComponent},
+      {path: 'contact', component: ContactComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent}
 ];
 @NgModule({
       imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
