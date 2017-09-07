8eb22ee061d31507d27ccc8d26693b4f890339ab
simzzz <swagb3ast@hotmail.com>
2017-09-07 14:46:30 +0300

    Navigation now functions with routerLink

--- KingOfTheSports/src/app/components/nav-bar/nav-bar.module.ts
+++ KingOfTheSports/src/app/components/nav-bar/nav-bar.module.ts
@@ -1,10 +1,12 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   declarations: [NavBarComponent]
 })
