e70eac38e3d0ad21566b28e42e04f8a04a0aad62
angel75013 <angelyazarov@yahoo.com>
2017-09-09 09:20:06 +0300

    all models

---
+++ KingOfTheSports/src/models/player.ts
@@ -0,0 +1,11 @@
+export class Player {
+    id: number;
+    name: string;
+    position: string;
+    dateOfBirth: Date;
+    jerseyNumber: number;
+    nationality: string;
+    contractUntil: number;
+    marketValue: number;
+    
+}
