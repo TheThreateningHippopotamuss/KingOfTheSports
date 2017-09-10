a51c25d8011fa275d6dcc3fe61109a8b2d6c563c
MarianNikolov <marian.nikolov.net@gmail.com>
2017-09-11 00:12:31 +0300

    Registered pipes

--- KingOfTheSports/src/app/shared/shared.module.ts
+++ KingOfTheSports/src/app/shared/shared.module.ts
@@ -1,3 +1,5 @@
+import { SortPlayersByNumberPipe } from './../pipes/sortPlayersByNumber.pipe';
+import { FilterLeaguesPipe } from './../pipes/filterLeagues.pipe';
 import { FooterComponent } from './footer/footer.component';
 import { LoaderComponent } from './loader/loader.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -5,14 +7,14 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthNavComponent } from "./authNav/authNav.component";
+import { AuthNavComponent } from './authNav/authNav.component';
 import { FormsModule } from '@angular/forms/';
 
 @NgModule({
   imports: [
-    CommonModule,  FormsModule,
+    CommonModule, FormsModule,
   ],
-  declarations: [LoaderComponent,AuthNavComponent,FooterComponent,NotFoundComponent],
-  exports: [LoaderComponent, CommonModule,AuthNavComponent,FooterComponent,NotFoundComponent]
+  declarations: [LoaderComponent, AuthNavComponent, FooterComponent, NotFoundComponent, FilterLeaguesPipe, SortPlayersByNumberPipe],
+  exports: [LoaderComponent, CommonModule, AuthNavComponent, FooterComponent, NotFoundComponent, FilterLeaguesPipe, SortPlayersByNumberPipe]
 })
 export class SharedModule { }
