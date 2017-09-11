83c26b5494f3cdeb02a05670242842775e958bdd
MarianNikolov <marian.nikolov.net@gmail.com>
2017-09-11 07:42:41 +0300

    Added loader unit test

---
+++ KingOfTheSports/src/app/shared/loader/loader.component.spec.ts
@@ -0,0 +1,33 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { LoaderComponent } from './loader.component';
+
+describe('LoaderComponent', () => {
+    let component: LoaderComponent;
+    let fixture: ComponentFixture<LoaderComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoaderComponent]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should be created correct instance', () => {
+        expect(component).toBeTruthy();
+    });
+
+    //   it('should render correct text', () => {
+    //     let de = fixture.debugElement.query(By.css('h3'));
+    //     let el: HTMLElement = de.nativeElement;
+
+    //     expect(el.innerText).toContain('data');
+    // });
+});
